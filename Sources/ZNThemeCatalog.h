f84cacbc6f95a974b234438186dee8eeba61f222
a7987083 <85878547+a7987083@users.noreply.github.com>
2026-09-18 03:10:34 +0800

    feat(ui): add 20-style neon theme catalog interface

---
+++ Sources/ZNThemeCatalog.h
@@ -0,0 +1,18 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+FOUNDATION_EXPORT NSString * const ZNThemeNameKey;
+FOUNDATION_EXPORT NSString * const ZNThemePrimaryKey;
+FOUNDATION_EXPORT NSString * const ZNThemeSecondaryKey;
+FOUNDATION_EXPORT NSString * const ZNThemeSurfaceKey;
+FOUNDATION_EXPORT NSString * const ZNThemeSurfaceAltKey;
+FOUNDATION_EXPORT NSString * const ZNThemeTextKey;
+FOUNDATION_EXPORT NSString * const ZNThemeMutedKey;
+FOUNDATION_EXPORT NSString * const ZNThemeCornerKey;
+FOUNDATION_EXPORT NSString * const ZNThemeBorderKey;
+FOUNDATION_EXPORT NSString * const ZNThemeShadowKey;
+
+FOUNDATION_EXPORT NSArray<NSDictionary<NSString *, id> *> *ZNThemeCatalog(void);
+
+NS_ASSUME_NONNULL_END
